b95bfac4c0cf9ca40cb0fadce9dc9c5ee2cc03ee
Uwe L. Korn <uwelk@xhochy.com>
2019-12-28 08:53:57 +0100

    Use CMake for building

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
 
-
-### Build and check
-
-make
-make check
-
-
-### Install manually ( no make install :) )
-
-# Ensure there is somewhere to put this stuff
-mkdir -p $PREFIX/include
-mkdir -p $PREFIX/lib
-
-# Move over the libs
-mv out-shared/libleveldb.* $PREFIX/lib/
-mv out-static/lib{leveldb,memenv}.a $PREFIX/lib/
-
-# Move over the includes
-mv include/leveldb $PREFIX/include/
+mkdir build
+pushd build
+cmake -GNinja -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=$PREFIX ..
+ninja install
+popd
